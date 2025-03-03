d056e164d24b79b9857c510ecc62125b4faa24f9
kuzchi <40554757+kuzchi@users.noreply.github.com>
2025-03-03 15:07:16 +0300

    Add files via upload

---
+++ dbconfig.py
@@ -0,0 +1,10 @@
+{
+    "telegram_bot_token": "ВАШ_ТОКЕН_БОТА",
+    "db_config": {
+        "user": "postgres",
+        "password": "1305",
+        "database": "ForBot",
+        "host": "localhost",
+        "port": 5432
+    }
+}
